f5ef0be27b878e0269cd2430fdb50c01430d49ce
Matt Davey <matt.davey@fsfe.org>
2022-12-02 22:50:19 +0000

    Day1 solution

---
+++ Day1/Program.fs
@@ -0,0 +1,31 @@
+﻿open System.IO
+
+// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+File.ReadAllLines("./input.txt")
+|> Array.fold (fun accumulator item ->
+    match item with
+    | "" -> []::accumulator
+    | _ -> match accumulator with
+           | [] -> [[item]]
+           | head::tail -> (item::head)::tail
+) []
+|> List.map (List.map int)
+|> List.map List.sum
+|> List.max
+|> printfn "%i"
+
+// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+File.ReadAllLines("./input.txt")
+|> Array.fold (fun accumulator item ->
+    match item with
+    | "" -> []::accumulator
+    | _ -> match accumulator with
+           | [] -> [[item]]
+           | head::tail -> (item::head)::tail
+) []
+|> List.map (List.map int)
+|> List.map List.sum
+|> List.sortDescending
+|> List.take 3
+|> List.sum
+|> printfn "%i"
