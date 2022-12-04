1913d4d157d76aa235be100d369b185b226496d2
Matt Davey <matt.davey@fsfe.org>
2022-12-04 21:32:09 +0000

    Don't check set intersection twice, if `a` intersects `b` then `b` *must* intersect `a`

--- Day4/Part2.fs
+++ Day4/Part2.fs
@@ -6,12 +6,13 @@ module Set = let intersects a b = Set.intersect a b |> Set.count > 0
 let parseRange x =
     let xs = String.split '-' x
     Set.ofSeq { (int xs[0])..(int xs[1]) }
+
 let parseLine x =
     let xs = String.split ',' x
     (parseRange xs[0], parseRange xs[1])
 
 let run : string array -> unit =
     Array.map parseLine
-    >> Array.filter (fun (a, b) -> Set.intersects a b || Set.intersects b a)
+    >> Array.filter (fun (a, b) -> Set.intersects a b)
     >> Array.length
     >> printfn "%i"
