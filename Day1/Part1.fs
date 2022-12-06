9a9eb080891c9b03237b63ed9b9be7c3a7d19ad7
Matt Davey <matt.davey@fsfe.org>
2022-12-06 08:24:35 +0000

    Avoid excessive allocation of arrays in Day1 solution

--- Day1/Part1.fs
+++ Day1/Part1.fs
@@ -1,10 +1,10 @@
 module Day1.Part1
 
-let run : string array -> unit =
-    Array.map (function
+let run : string seq -> unit =
+    Seq.map (function
         | "" -> fun (xs, acc) -> (acc::xs, 0)
         | x -> fun (xs, acc) -> (xs, acc + int x)
     )
-    >> Array.fold (fun acc fn -> fn acc) ([], 0)
+    >> Seq.fold (fun acc fn -> fn acc) ([], 0)
     >> fun (xs, acc) -> List.max (acc::xs)
     >> printfn "%i"
