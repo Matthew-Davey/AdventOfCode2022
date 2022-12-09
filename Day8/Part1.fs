86011a2b10a39247f850265de79a885990bebd84
Matthew Davey <matthew.davey@vonage.com>
2022-12-09 19:01:27 +0000

    Refactor Day8 solution

--- Day8/Part1.fs
+++ Day8/Part1.fs
@@ -5,30 +5,28 @@ module Array2D =
         [| for x in [0..Array2D.length1 xy-1] do
             yield [| for y in [0..Array2D.length2 xy-1] -> xy[x, y] |]
         |]
-    let mapr<'a> fn (xy: 'a[,]) =
-        Array2D.mapi (fun x y v -> fn y xy[x, *]) xy
+    let mapRows fn = toJagged >> (Seq.map fn)
+    let mapr<'a> fn (xy: 'a[,]) = Array2D.mapi (fun x y v -> fn y v xy[x, *]) xy
     let transpose<'a> = toJagged >> Array.transpose >> array2D
-    let zip<'a, 'b, 'c> (a : 'a[,]) (b : 'b[,]) (fn : 'a -> 'b -> 'c) =
-        Array2D.mapi (fun x y v -> fn v b[x, y]) a
+    let combine<'a, 'b, 'c> (a : 'a[,]) (b : 'b[,]) (fn : 'a -> 'b -> 'c) = Array2D.mapi (fun x y v -> fn v b[x, y]) a
 
 let loadForest =
     Seq.map (Seq.map int)
     >> Seq.map (Seq.map (fun x -> x - 48))
     >> array2D
 
-let visibleFromOutside i (xs : int array) =
+let visibleFromOutside i v (xs : int array) =
     match i with
     | 0 -> 1
     | i when i = Seq.length xs - 1 -> 1
-    | i -> if xs[i] > Seq.max xs[..i-1] || xs[i] > Seq.max xs[i+1..] then 1 else 0
+    | i -> if v > Seq.max xs[..i-1] || v > Seq.max xs[i+1..] then 1 else 0
 
 let run (input : string array) =
     let forest = loadForest input
     let h = forest |> Array2D.mapr visibleFromOutside
     let v = forest |> Array2D.transpose |> Array2D.mapr visibleFromOutside |> Array2D.transpose
 
-    Array2D.zip h v max
-    |> Array2D.toJagged
-    |> Seq.map (Seq.sum)
+    Array2D.combine h v max
+    |> Array2D.mapRows (Seq.sum)
     |> Seq.sum
     |> printfn "%A"
--- Day8/Part2.fs
+++ Day8/Part2.fs
@@ -5,11 +5,10 @@ module Array2D =
         [| for x in [0..Array2D.length1 xy-1] do
             yield [| for y in [0..Array2D.length2 xy-1] -> xy[x, y] |]
         |]
-    let mapr<'a> fn (xy: 'a[,]) =
-        Array2D.mapi (fun x y v -> fn y xy[x, *]) xy
+    let mapRows fn = toJagged >> (Seq.map fn)
+    let mapr<'a> fn (xy: 'a[,]) = Array2D.mapi (fun x y v -> fn y v xy[x, *]) xy
     let transpose<'a> = toJagged >> Array.transpose >> array2D
-    let zip<'a, 'b, 'c> (a : 'a[,]) (b : 'b[,]) (fn : 'a -> 'b -> 'c) =
-        Array2D.mapi (fun x y v -> fn v b[x, y]) a
+    let combine<'a, 'b, 'c> (a : 'a[,]) (b : 'b[,]) (fn : 'a -> 'b -> 'c) = Array2D.mapi (fun x y v -> fn v b[x, y]) a
 
 let loadForest =
     Seq.map (Seq.map int)
@@ -23,13 +22,13 @@ let takeWhilePrev fn =
         | xs -> if fn (Seq.head xs) then i::acc else acc
     ) []
 
-let countVisibleTrees i (xs : int array) =
+let countVisibleTrees i v (xs : int array) =
     match i with
     | 0 ->  0
     | i when i = (Seq.length xs) - 1 -> 0
     | i ->
-        let visibleLeft = xs[..i-1] |> Seq.rev |> takeWhilePrev (fun x -> x < xs[i])
-        let visibleRight = xs[i+1..] |> takeWhilePrev (fun x -> x < xs[i])
+        let visibleLeft = xs[..i-1] |> Seq.rev |> takeWhilePrev (fun x -> x < v)
+        let visibleRight = xs[i+1..] |> takeWhilePrev (fun x -> x < v)
         Seq.length visibleLeft * Seq.length visibleRight
 
 let run (input : string array) =
@@ -37,8 +36,7 @@ let run (input : string array) =
     let h = forest |> Array2D.mapr countVisibleTrees
     let v = forest |> Array2D.transpose |> Array2D.mapr countVisibleTrees |> Array2D.transpose
 
-    Array2D.zip h v (fun a b -> match a, b with 0, 0 -> 0 | a, b -> a * b)
-    |> Array2D.toJagged
-    |> Seq.map (Seq.max)
+    Array2D.combine h v (fun a b -> match a, b with 0, 0 -> 0 | a, b -> a * b)
+    |> Array2D.mapRows (Seq.max)
     |> Seq.max
     |> printfn "%A"
