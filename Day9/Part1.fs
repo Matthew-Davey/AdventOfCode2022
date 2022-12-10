8f6cc54e3bd8b51451263dd9e6d3a5ea130616f1
Matt Davey <matt.davey@fsfe.org>
2022-12-10 03:02:04 +0000

    Day9 solution

--- Day9/Part1.fs
+++ Day9/Part1.fs
@@ -1,6 +1,6 @@
 module Day9.Part1
 
-let moveTail (hx : int, hy : int) (tx : int, ty : int) =
+let moveTail (hx, hy) (tx, ty) =
     match (tx - hx, ty - hy) with
     | (0, 2) -> (hx, hy + 1)
     | (1, 2) -> (hx, hy + 1)
@@ -16,18 +16,17 @@ let moveTail (hx : int, hy : int) (tx : int, ty : int) =
     | (-1, 2) -> (hx, hy + 1)
     | _ -> (tx, ty)
 
-let run  (input : string array) =
-    printfn "Expect 6011"
-    input
-    |> Seq.collect (fun move -> Seq.replicate (int move[2..]) $"{move[0]} 1")
-    |> Seq.fold (fun ((hx, hy: int), hist) move ->
-        let (tx, ty) = List.head hist
-        match move with
-        | "R 1" -> (hx + 1, hy), (moveTail (hx + 1, hy) (tx, ty))::hist
-        | "L 1" -> (hx - 1, hy), (moveTail (hx - 1, hy) (tx, ty))::hist
-        | "U 1" -> (hx, hy + 1), (moveTail (hx, hy + 1) (tx, ty))::hist
-        | "D 1" -> (hx, hy - 1), (moveTail (hx, hy - 1) (tx, ty))::hist
+let run : string array -> unit =
+    Seq.collect (fun move -> Seq.replicate (int move[2..]) $"{move[0]} 1")
+    >> Seq.map (function
+    | "R 1" -> fun (hx, hy) -> (hx + 1, hy)
+    | "L 1" -> fun (hx, hy) -> (hx - 1, hy)
+    | "U 1" -> fun (hx, hy) -> (hx, hy + 1)
+    | "D 1" -> fun (hx, hy) -> (hx, hy - 1)
+    )
+    >> Seq.fold (fun (head, hist) move ->
+        (move head), (moveTail (move head) (List.head hist))::hist
     ) ((0, 0), [(0,0)])
-    |> fun (_, hist) -> Set.ofList hist
-    |> Set.count
-    |> printfn "%i"
+    >> fun (_, hist) -> Set.ofList hist
+    >> Set.count
+    >> printfn "%i"
